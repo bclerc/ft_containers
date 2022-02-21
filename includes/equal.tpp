ca6126d00ddeb00ea125e6c4fe8afeb761764482
Bryce CLERC <bclerc@e2r11p3.clusters.42paris.fr>
2022-02-21 17:22:00 +0100

    Lexicograpgical_compare (gros mot pour pas grand chose, le cpp quoi.)

--- includes/equal.tpp
+++ includes/equal.tpp
@@ -6,7 +6,7 @@
 namespace ft 
 {
 	template <class InputIterator1, class InputIterator2>
-	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+	bool	equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
 	{
 		while (first1 != last1)
 		{
@@ -19,7 +19,7 @@ namespace ft
 	}
 
 	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-	bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+	bool	equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
 	{
 		while (first1 != last1)
 		{
@@ -29,6 +29,40 @@ namespace ft
 			first2++;
 		}
 		return true;
+	}
+
+	template<class InputIterator1, class InputIterator2>
+	bool	lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (*first1 < *first2)
+				return true;
+			first1++;
+			first2++;
+		}
+		return (first2 != last2);
+	}
+
+
+	template<class InputIterator1, class InputIterator2, class Compare>
+	bool	lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+									InputIterator2 first2, InputIterator2 last2,
+									Compare comp)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
+			else if (comp(*first1, *first2))
+				return true;
+			first1++;
+			first2++;
+		}
+		return (first2 != last2);
 	}
 };
 
--- includes/main.cpp
+++ includes/main.cpp
@@ -1,29 +1,55 @@
 #include "pair.tpp"
 #include "equal.tpp"
 #include <vector>
+#include <iostream>     // std::cout
+#include <algorithm>    // std::equal
+#include <vector>  
+
+bool mypredicate (int i, int j) {
+  return (i==j);
+}
+
+bool mycomp (char c1, char c2)
+{ return std::tolower(c1)<std::tolower(c2); }
+
 
 int main(void)
 {
 	ft::pair<int, int> yo{10, 20};
-	std::cout << yo.first << " " << yo.second;
-
-	std::vector<int> tab;
-	std::vector<int> tab2;
-
-	tab.push_back(5);
-	tab.push_back(4);
-	tab.push_back(3);
-	tab.push_back(2);
-
-	
-	tab2.push_back(5);
-	tab2.push_back(8);
-	tab2.push_back(3);
-	tab2.push_back(2);
+	std::cout << yo.first << " " << yo.second << std::endl;
 
 
-	if (std::equal(tab.begin(), tab.end(), tab2.begin()))
-		std::cout << "Equal" << std::endl;
+  int myints[] = {20,40,60,80,100};               //   myints: 20 40 60 80 100
+  std::vector<int>myvector (myints,myints+5);     // myvector: 20 40 60 80 100
+
+  // using default comparison:
+  if ( ft::equal (myvector.begin(), myvector.end(), myints) )
+    std::cout << "The contents of both sequences are equal.\n";
+  else
+    std::cout << "The contents of both sequences differ.\n";
+
+  myvector[3]=81;                                 // myvector: 20 40 60 81 100
+
+  // using predicate comparison:
+  if ( ft::equal (myvector.begin(), myvector.end(), myints, mypredicate) )
+    std::cout << "The contents of both sequences are equal.\n";
+  else
+    std::cout << "The contents of both sequences differ.\n";
+
+	  char foo[]="Apple";
+  char bar[]="apartment";
+
+  std::cout << std::boolalpha;
+
+  std::cout << "Comparing foo and bar lexicographically (foo<bar):\n";
+
+  std::cout << "Using default comparison (operator<): ";
+  std::cout << ft::lexicographical_compare(foo,foo+5,bar,bar+9);
+  std::cout << '\n';
+
+  std::cout << "Using mycomp as comparison object: ";
+  std::cout << ft::lexicographical_compare(foo,foo+5,bar,bar+9,mycomp);
+  std::cout << '\n';
 
 	return 0;
 }
