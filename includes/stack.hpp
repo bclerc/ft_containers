f9f01a5a3963759bcf8854459d7767ca6c0be72a
Bryce CLERC <bclerc@e2r2p13.clusters.42paris.fr>
2022-03-20 02:16:26 +0100

    Fix stack

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 14:56:08 by bclerc            #+#    #+#             */
-/*   Updated: 2022/03/14 21:24:47 by bclerc           ###   ########.fr       */
+/*   Updated: 2022/03/20 02:14:00 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,19 @@ namespace ft {
 
 		public:
 			typedef Container					container_type;
-			typedef	Container::value_type		value_type;
-			typedef Container::size_type		size_type;
-			typedef Container::reference		reference;
-			typedef Container::const_reference	const_reference;
+			typedef	typename Container::value_type		value_type;
+			typedef typename Container::size_type		size_type;
+			typedef typename Container::reference		reference;
+			typedef typename Container::const_reference	const_reference;
 
 		private: 
 			Container container;
 
 		public :
 
-			stack (void) 
+			explicit stack( const Container& cont = Container())
 			{
-				container = Container();
-			}
-
-			explicit stack( const Container& cont = Container());
-			{
-				container = cont();
+				container = cont;
 			}
 
 			stack (const stack & other)
@@ -47,10 +42,7 @@ namespace ft {
 				container = Container(other);
 			}
 
-			~stack(void)
-			{
-				container::~Container();
-			};
+			~stack(void){};
 
 			stack& operator=( const stack & other )
 			{
@@ -91,46 +83,38 @@ namespace ft {
 			{
 				container.pop_back();
 			}
+
+			friend bool operator==( const stack<T,Container>& lhs, const stack<T,Container>& rhs )
+			{
+				return (lhs.container == rhs.container);
+			}
+
+			friend bool operator!= (const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+			{
+				return (lhs.container != rhs.container);
+			}
+
+			friend bool operator<  (const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+			{
+				return (lhs.container < rhs.container);
+			}
+
+			friend bool operator<= (const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+			{
+				return (lhs.container <= rhs.container);
+			}
+
+			friend bool operator> (const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+			{
+				return (lhs.container > rhs.container);
+			}
+
+			friend bool operator>= (const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+			{
+				return (lhs.container >= rhs.container);
+			}
 	};
 
-	template <class T>
-  	bool operator== (const stack<T>& lhs, const stack<T>& rhs)
-	{
-		if (rhs.begin() == rhs.end() && lhs.begin() != lhs.end())
-			return false;
-		return (ft::equal(lhs.begin(), lhs.end(), rhs.begin()));
-	}
-
-	template <class T>
-  	bool operator!= (const stack<T>& lhs, const stack<T>& rhs)
-	{
-		return (!(lhs == rhs));
-	}
-
-	template <class T>
-  	bool operator<  (const stack<T>& lhs, const stack<T>& rhs)
-	{
-		
-		return (ft::lexicographical_compare(lhs.begin(), lhs.end(),
-											rhs.begin(), rhs.end()));
-	}
-
-	template <class T>
- 	bool operator<= (const stack<T>& lhs, const stack<T>& rhs)
-	{
-		return (!(lhs > rhs));
-	}
-	template <class T>
-  	bool operator> (const stack<T>& lhs, const stack<T>& rhs)
-  	{
-		return (!(lhs < rhs));
-  	}
-
-	template <class T>
-  	bool operator>= (const stack<T>& lhs, const stack<T>& rhs)
-  	{
-		  return !(lhs < rhs);
-  	}
 };
 
 #endif
