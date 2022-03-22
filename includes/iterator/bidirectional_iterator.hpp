e14255db5e038f3aa39407728ab1c4c4024ec005
Bryce CLERC <bclerc@e2r4p23.clusters.42paris.fr>
2022-03-22 02:59:51 +0100

    start of map

---
+++ includes/iterator/bidirectional_iterator.hpp
@@ -0,0 +1,185 @@
+/* *** *********************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bidirectional_iterator.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 20:21:35 by bclerc            #+#    #+#             */
+/*   Updated: 2022/02/23 20:47:38 by bclerc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#ifndef bidirectional_iterator_HPP
+# define bidirectional_iterator_HPP
+
+#include "iterator.hpp"
+
+namespace ft {
+
+	template <class T>
+	class bidirectional_iterator : public ft::iterator<ft::bidirectional_iterator_tag, T> {
+
+		public:
+			typedef typename ft::iterator<ft::bidirectional_iterator_tag, T>::value_type		iterator_type;
+			typedef typename ft::iterator<ft::bidirectional_iterator_tag, T>::difference_type	difference_type;
+			typedef T*																			pointer;
+			typedef T&																			reference;
+			
+
+		private:
+			pointer _base;
+
+
+		public:
+			bidirectional_iterator(void) : _base(NULL) {}
+			bidirectional_iterator(pointer it) : _base(it) {}
+				
+			bidirectional_iterator (const bidirectional_iterator & rev_it) : _base(rev_it.base())
+			{
+				return ;
+			}
+		
+			virtual ~bidirectional_iterator() {}
+
+			pointer base() const
+			{
+				return (this->_base);
+			}
+
+			reference operator*() const
+			{
+				return (*this->_base);
+			}
+
+
+			bidirectional_iterator &operator=(const bidirectional_iterator & rhs)
+			{
+				if (this == &rhs)
+					return (*this);
+				this->_base = (rhs._base);
+				return *this;
+			}
+
+
+			bidirectional_iterator operator+(difference_type n) const
+			{
+				T *tmp;
+				tmp = _base + n;
+				return (bidirectional_iterator(tmp));
+			}
+
+			bidirectional_iterator & operator++()
+			{
+				++_base;
+				return(*this);
+			}
+
+			bidirectional_iterator operator++(int)
+			{
+				bidirectional_iterator tmp = *this;
+				++(*this);
+				return (tmp);
+			}
+
+			bidirectional_iterator operator+=(difference_type n)
+			{
+				_base += n;
+				return (*this);
+			}
+
+			bidirectional_iterator operator-(difference_type n) const
+			{
+				T *tmp;
+				tmp = _base - n;
+				return (bidirectional_iterator(tmp));
+			}
+
+			bidirectional_iterator & operator--() 
+			{
+				--_base;
+				return(*this);
+			}
+
+			bidirectional_iterator operator--(int)
+			{
+				bidirectional_iterator tmp = *this;
+				--(*this);
+				return (tmp);
+			}
+
+			bidirectional_iterator operator-=(difference_type n)
+			{
+				_base -= n;
+				return (*this);
+			}
+
+			pointer operator->() const
+			{
+				return &(operator*());
+			}
+
+			reference operator[] (difference_type n) const
+			{
+				return (*(_base + n));
+			}
+
+            operator bidirectional_iterator<const T> () const
+            {
+				 return (bidirectional_iterator<const T>(this->_base));
+			}
+	};
+
+	template <class T, class Other>
+	bool operator==(ft::bidirectional_iterator<T> const & lhs, ft::bidirectional_iterator<Other> const& rhs)
+	{
+		return (lhs.base() == rhs.base());	
+	}
+
+	template <class T, class Other>
+	bool operator!=(ft::bidirectional_iterator<T> const & lhs, ft::bidirectional_iterator<Other> const& rhs)
+	{
+		return (lhs.base() != rhs.base());	
+	}
+
+	template <class T, class Other>
+	bool operator<(ft::bidirectional_iterator<T> const & lhs, ft::bidirectional_iterator<Other> const& rhs)
+	{
+		return (lhs.base() < rhs.base());	
+	}
+
+	template <class T, class Other>
+	bool operator<=(ft::bidirectional_iterator<T> const & lhs, ft::bidirectional_iterator<Other> const& rhs)
+	{
+		return (lhs.base() <= rhs.base());	
+	}
+
+	template <class T, class Other>
+	bool operator>(ft::bidirectional_iterator<T> const & lhs, ft::bidirectional_iterator<Other> const& rhs)
+	{
+		return (lhs.base() > rhs.base());	
+	}
+
+	template <class T, class Other>
+	bool operator>=(ft::bidirectional_iterator<T> const & lhs, ft::bidirectional_iterator<Other> const& rhs)
+	{
+		return (lhs.base() >= rhs.base());	
+	}
+
+	template <class T>
+	bidirectional_iterator<T> operator+(typename bidirectional_iterator<T>::difference_type n, 
+		const bidirectional_iterator<T> & rev_it)
+	{
+		return (rev_it + n);		
+	}
+
+	template <class T, class O>
+	typename bidirectional_iterator<T>::difference_type operator- (bidirectional_iterator<T> const & lhs, bidirectional_iterator<O>  const & rhs)
+	{
+		return (lhs.base() - rhs.base());
+	}
+};
+
+#endif
---
+++ includes/map.hpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 01:48:13 by bclerc            #+#    #+#             */
+/*   Updated: 2022/03/22 02:59:18 by bclerc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_HPP
+# define MAP_HPP
+
+#include <memory>
+#include "equal.hpp"
+#include "iterator/reverse_iterator.hpp"
+#include "bidirectional_iterator.hpp"
+
+
+namespace ft 
+{
+	template<
+		class Key,
+		class T,
+		class Compare = std::less<Key>,
+		class Allocator = std::allocator<std::pair<const Key, T> >
+	>
+	class map
+	{
+		public:
+			typedef Key											key_type;
+			typedef T											mapped_type;
+			typedef	value_type									ft::pair<const Key, T>
+			typedef std::size_t									size_type;
+			typedef std::ptrdiff_t								difference_type;
+			typedef	Compare										key_compare;
+			typedef allocator_type								Allocator;
+			typedef value_type &								reference;
+			typedef const value_type & 							const_reference;
+			typedef	Allocator::pointer							pointer;
+			typedef Allocator::const_pointer					const_pointer;
+			typedef typename bidirectional_iterator<T>			iterator;
+			typedef typename bidirectional_iterator<const T>	const_iterator;
+			typedef reverse_iterator<Iterator>					reverse_iterator;
+			typedef	reverse_iterator<const_iterator>			const_reverse_iterator;
+		
+		private:
+			Allocator _alloc;
+			size_type _size;
+			size_type _capacity;
+
+		public:
+			map();
+
+			explicit map( const Compare& comp,
+						const Allocator& alloc = Allocator() );
+			template< class InputIt >
+			map( InputIt first, InputIt last, const Compare& comp = Compare(), const Allocator& alloc = Allocator() );
+			map( const map& other );
+			~map();
+			map& operator=( const map& other );
+			allocator_type get_allocator() const
+			{
+				return _alloc;
+			}
+			T& at( const Key& key );
+			const T& at( const Key& key ) const;
+			T& operator[]( const Key& key );
+			iterator begin();
+			iterator end();
+			reverse_iterator rbegin();
+			const_reverse_iterator rbegin() const;
+			reverse_iterator rend();
+			const_reverse_iterator rend() const;
+
+			bool empty() const
+			{
+				return !(_size > 0);
+			}
+
+			size_type size() const
+			{
+				return _size;
+			}
+
+			size_type max_size() const
+			{
+				return (this->_alloc.max_size()));
+			}
+
+			void clear();
+			ft::pair<iterator, bool> insert( const value_type& value );
+			iterator insert( iterator hint, const value_type& value );
+			void erase( iterator pos );
+			void erase( iterator first, iterator last );
+			size_type erase( const Key& key );
+			void swap( map& other );
+			size_type count( const Key& key ) const;
+			iterator find( const Key& key );
+			const_iterator find( const Key& key ) const;
+			ft::pair<iterator,iterator> equal_range( const Key& key );
+			ft::pair<const_iterator,const_iterator> equal_range( const Key& key ) const;
+			iterator lower_bound( const Key& key );
+			const_iterator lower_bound( const Key& key ) const;
+			iterator upper_bound( const Key& key );
+			const_iterator upper_bound( const Key& key ) const;
+	}
+};
+
+#endif
