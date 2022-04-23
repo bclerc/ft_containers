8ec8232aa6e93e774422c8bdacd198fea4783f4c
BuildTools <unconfigured@null.spigotmc.org>
2022-04-23 21:29:21 +0000

    Test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,45 +1,40 @@
-#include "pair.tpp"
-#include "stack.hpp"
-#include "vector.hpp"
+#include "../includes/stack.hpp"
+#include "../includes/vector.hpp"
 #include <iostream>    
 #include <algorithm>
 #include <vector>
+#include <stack>
 #include <map>
 
-
-
-#define TESTED_TYPE int
 #define TESTED_NAMESPACE ft
-
 void construct_test()
 {
 	// constructors used in the same order as described above:
-  std::vector<int> first;                                // empty vector of ints
-  std::vector<int> second (4,100);                       // four ints with value 100
-  std::vector<int> third (second.begin(),second.end());  // iterating through second
-  std::vector<int> fourth (third);                       // a copy of third
+  TESTED_NAMESPACE::vector<int> first;                                // empty vector of ints
+  TESTED_NAMESPACE::vector<int> second (4,100);                       // four ints with value 100
+  TESTED_NAMESPACE::vector<int> third (second.begin(),second.end());  // iterating through second
+  TESTED_NAMESPACE::vector<int> fourth (third);                       // a copy of third
 
   // the iterator constructor can also be used to construct from arrays:
   int myints[] = {16,2,77,29};
-  std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+  TESTED_NAMESPACE::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
 
   std::cout << "The contents of fifth are:";
-  for (std::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+  for (TESTED_NAMESPACE::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
     std::cout << ' ' << *it;
   std::cout << '\n';
 
-  return 0;
 }
 
 void assign_test()
 {
-  std::vector<int> first;
-  std::vector<int> second;
-  std::vector<int> third;
+  TESTED_NAMESPACE::vector<int> first;
+  TESTED_NAMESPACE::vector<int> second;
+  TESTED_NAMESPACE::vector<int> third;
 
   first.assign (7,100);             // 7 ints with a value of 100
 
-  std::vector<int>::iterator it;
+  TESTED_NAMESPACE::vector<int>::iterator it;
   it=first.begin()+1;
 
   second.assign (it,first.end()-1); // the 5 central values of first
@@ -50,12 +45,12 @@ void assign_test()
   std::cout << "Size of first: " << int (first.size()) << '\n';
   std::cout << "Size of second: " << int (second.size()) << '\n';
   std::cout << "Size of third: " << int (third.size()) << '\n';
-  return 0;
+  
 }
 
 void at_test()
 {
- std::vector<int> myvector (10);   // 10 zero-initialized ints
+ TESTED_NAMESPACE::vector<int> myvector (10);   // 10 zero-initialized ints
 
   // assign some values:
   for (unsigned i=0; i<myvector.size(); i++)
@@ -66,12 +61,12 @@ void at_test()
     std::cout << ' ' << myvector.at(i);
   std::cout << '\n';
 
-  return 0;
+  
 }
 
 void back_test()
 {
-std::vector<int> myvector;
+TESTED_NAMESPACE::vector<int> myvector;
   myvector.push_back(10);
 
   while (myvector.back() != 0)
@@ -84,12 +79,12 @@ std::vector<int> myvector;
     std::cout << ' ' << myvector[i];
   std::cout << '\n';
 
-  return 0;
+  
 }
 
 void capacity_test()
 {
-	std::vector<int> myvector;
+	TESTED_NAMESPACE::vector<int> myvector;
 
   // set some content in the vector:
   for (int i=0; i<100; i++) myvector.push_back(i);
@@ -97,12 +92,12 @@ void capacity_test()
   std::cout << "size: " << (int) myvector.size() << '\n';
   std::cout << "capacity: " << (int) myvector.capacity() << '\n';
   std::cout << "max_size: " << (int) myvector.max_size() << '\n';
-  return 0;
+  
 }
 
 void cbegin_test()
 {
-	 std::vector<int> myvector;
+	 TESTED_NAMESPACE::vector<int> myvector;
 
   // set some content in the vector:
   for (int i=0; i<100; i++) myvector.push_back(i);
@@ -110,12 +105,12 @@ void cbegin_test()
   std::cout << "size: " << (int) myvector.size() << '\n';
   std::cout << "capacity: " << (int) myvector.capacity() << '\n';
   std::cout << "max_size: " << (int) myvector.max_size() << '\n';
-  return 0;
+  
 }
 
 void clear_test()
 {
-	 std::vector<int> myvector;
+	 TESTED_NAMESPACE::vector<int> myvector;
   myvector.push_back (100);
   myvector.push_back (200);
   myvector.push_back (300);
@@ -139,7 +134,7 @@ void clear_test()
 void empty_test()
 {
 	
-	 std::vector<int> myvector;
+	 TESTED_NAMESPACE::vector<int> myvector;
   int sum (0);
 
   for (int i=1;i<=10;i++) myvector.push_back(i);
@@ -156,18 +151,18 @@ void empty_test()
 
 void end_test()
 {
-	  std::vector<int> myvector;
+	  TESTED_NAMESPACE::vector<int> myvector;
   for (int i=1; i<=5; i++) myvector.push_back(i);
 
   std::cout << "myvector contains:";
-  for (std::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
+  for (TESTED_NAMESPACE::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
     std::cout << ' ' << *it;
   std::cout << '\n';
 }
 
 void erase_test()
 {
-	 std::vector<int> myvector;
+	TESTED_NAMESPACE::vector<int> myvector;
 
   // set some values (from 1 to 10)
   for (int i=1; i<=10; i++) myvector.push_back(i);
@@ -186,7 +181,7 @@ void erase_test()
 
 void front_test()
 {
-	 std::vector<int> myvector;
+	 TESTED_NAMESPACE::vector<int> myvector;
 
   myvector.push_back(78);
   myvector.push_back(16);
@@ -201,7 +196,7 @@ void front_test()
 
 void allocator_test()
 {
-	  std::vector<int> myvector;
+	  TESTED_NAMESPACE::vector<int> myvector;
   int * p;
   unsigned int i;
 
@@ -222,8 +217,8 @@ void allocator_test()
 
 void insert_test()
 {
-	 std::vector<int> myvector (3,100);
-  std::vector<int>::iterator it;
+	 TESTED_NAMESPACE::vector<int> myvector (3,100);
+  TESTED_NAMESPACE::vector<int>::iterator it;
 
   it = myvector.begin();
   it = myvector.insert ( it , 200 );
@@ -233,7 +228,7 @@ void insert_test()
   // "it" no longer valid, get a new one:
   it = myvector.begin();
 
-  std::vector<int> anothervector (2,400);
+  TESTED_NAMESPACE::vector<int> anothervector (2,400);
   myvector.insert (it+2,anothervector.begin(),anothervector.end());
 
   int myarray [] = { 501,502,503 };
@@ -245,38 +240,316 @@ void insert_test()
   std::cout << '\n';
 }
 
+void operator_equal_test()
+{
+  TESTED_NAMESPACE::vector<int> foo (3,0);
+  TESTED_NAMESPACE::vector<int> bar (5,0);
+
+  bar = foo;
+  foo = TESTED_NAMESPACE::vector<int>();
+
+  std::cout << "Size of foo: " << int(foo.size()) << '\n';
+  std::cout << "Size of bar: " << int(bar.size()) << '\n';
+
+}
+
+void max_size_test()
+{
+	 TESTED_NAMESPACE::vector<int> myvector;
+
+  // set some content in the vector:
+  for (int i=0; i<100; i++) myvector.push_back(i);
+
+  std::cout << "size: " << myvector.size() << "\n";
+  std::cout << "capacity: " << myvector.capacity() << "\n";
+  std::cout << "max_size: " << myvector.max_size() << "\n";
+  
+}
+
+void operator_cage_test()
+{
+	 TESTED_NAMESPACE::vector<int> myvector (10);   // 10 zero-initialized elements
+
+  TESTED_NAMESPACE::vector<int>::size_type sz = myvector.size();
+
+  // assign some values:
+  for (unsigned i=0; i<sz; i++) myvector[i]=i;
+
+  // reverse vector using operator[]:
+  for (unsigned i=0; i<sz/2; i++)
+  {
+    int temp;
+    temp = myvector[sz-1-i];
+    myvector[sz-1-i]=myvector[i];
+    myvector[i]=temp;
+  }
+
+  std::cout << "myvector contains:";
+  for (unsigned i=0; i<sz; i++)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+
+  
+}
+void pop_back_test()
+{
+
+ TESTED_NAMESPACE::vector<int> myvector;
+  int sum (0);
+  myvector.push_back (100);
+  myvector.push_back (200);
+  myvector.push_back (300);
+
+  while (!myvector.empty())
+  {
+    sum+=myvector.back();
+    myvector.pop_back();
+  }
+
+  std::cout << "The elements of myvector add up to " << sum << '\n';
+
+  
+}
+
+void push_back_test()
+{
+	 TESTED_NAMESPACE::vector<int> myvector;
+
+	for (int i = 0; i < 20; i ++)
+		myvector.push_back(i);
+
+  std::cout << "myvector stores " << int(myvector.size()) << " numbers.\n";
+
+  
+}
+
+void rbegin_test()
+{
+	  TESTED_NAMESPACE::vector<int> myvector (5);  // 5 default-constructed ints
+
+  int i=0;
+
+  TESTED_NAMESPACE::vector<int>::reverse_iterator rit = myvector.rbegin();
+  for (; rit!= myvector.rend(); ++rit)
+    *rit = ++i;
+
+  std::cout << "myvector contains:";
+  for (TESTED_NAMESPACE::vector<int>::iterator it = myvector.begin(); it != myvector.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  
+}
+
+void rend_test()
+{
+
+	 TESTED_NAMESPACE::vector<int> myvector (5);  // 5 default-constructed ints
+
+  TESTED_NAMESPACE::vector<int>::reverse_iterator rit = myvector.rbegin();
+
+  int i=0;
+  for (rit = myvector.rbegin(); rit!= myvector.rend(); ++rit)
+    *rit = ++i;
+
+  std::cout << "myvector contains:";
+  for (TESTED_NAMESPACE::vector<int>::iterator it = myvector.begin(); it != myvector.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  
+}
+
+void reserve_test()
+{
+	  TESTED_NAMESPACE::vector<int>::size_type sz;
+
+  TESTED_NAMESPACE::vector<int> foo;
+  sz = foo.capacity();
+  std::cout << "making foo grow:\n";
+  for (int i=0; i<100; ++i) {
+    foo.push_back(i);
+    if (sz!=foo.capacity()) {
+      sz = foo.capacity();
+      std::cout << "capacity changed: " << sz << '\n';
+    }
+  }
+
+  TESTED_NAMESPACE::vector<int> bar;
+  sz = bar.capacity();
+  bar.reserve(100);   // this is the only difference with foo above
+  std::cout << "making bar grow:\n";
+  for (int i=0; i<100; ++i) {
+    bar.push_back(i);
+    if (sz!=bar.capacity()) {
+      sz = bar.capacity();
+      std::cout << "capacity changed: " << sz << '\n';
+    }
+  }
+  
+}
+
+void resize_test()
+{
+	  TESTED_NAMESPACE::vector<int> myvector;
+
+  // set some initial content:
+  for (int i=1;i<10;i++) myvector.push_back(i);
+
+  myvector.resize(5);
+  myvector.resize(8,100);
+  myvector.resize(12);
+
+  std::cout << "myvector contains:";
+  for (size_t i=0;i<myvector.size();i++)
+    std::cout << ' ' << myvector[i];
+  std::cout << '\n';
+
+  
+}
+
+void size_test()
+{
+
+	  TESTED_NAMESPACE::vector<int> myints;
+  std::cout << "0. size: " << myints.size() << '\n';
+
+  for (int i=0; i<10; i++) myints.push_back(i);
+  std::cout << "1. size: " << myints.size() << '\n';
+
+  myints.insert (myints.end(),10,100);
+  std::cout << "2. size: " << myints.size() << '\n';
+
+  myints.pop_back();
+  std::cout << "3. size: " << myints.size() << '\n';
+
+  
+}
+
+void swap_test()
+{
+
+	 TESTED_NAMESPACE::vector<int> foo (3,100);   // three ints with a value of 100
+  TESTED_NAMESPACE::vector<int> bar (5,200);   // five ints with a value of 200
+
+  foo.swap(bar);
+
+  std::cout << "foo contains:";
+  for (unsigned i=0; i<foo.size(); i++)
+    std::cout << ' ' << foo[i];
+  std::cout << '\n';
+
+  std::cout << "bar contains:";
+  for (unsigned i=0; i<bar.size(); i++)
+    std::cout << ' ' << bar[i];
+  std::cout << '\n';
+}
+
+void stack_empty()
+{
+	 TESTED_NAMESPACE::stack<int> mystack;
+  int sum (0);
+
+  for (int i=1;i<=10;i++) mystack.push(i);
+
+  while (!mystack.empty())
+  {
+     sum += mystack.top();
+     mystack.pop();
+  }
+
+  std::cout << "total: " << sum << '\n';
+
+}
+
+void stack_pop()
+{
+	  TESTED_NAMESPACE::stack<int> mystack;
+
+  for (int i=0; i<5; ++i) mystack.push(i);
+
+  std::cout << "Popping out elements...";
+  while (!mystack.empty())
+  {
+     std::cout << ' ' << mystack.top();
+     mystack.pop();
+  }
+  std::cout << '\n';
+
+}
+
+void stack_push()
+{
+	 TESTED_NAMESPACE::stack<int> mystack;
+
+  for (int i=0; i<5; ++i) mystack.push(i);
+
+  std::cout << "Popping out elements...";
+  while (!mystack.empty())
+  {
+     std::cout << ' ' << mystack.top();
+     mystack.pop();
+  }
+  std::cout << '\n';
+
+}
+
+void stack_size()
+{
+ TESTED_NAMESPACE::stack<int> myints;
+  std::cout << "0. size: " << myints.size() << '\n';
+
+  for (int i=0; i<5; i++) myints.push(i);
+  std::cout << "1. size: " << myints.size() << '\n';
+
+  myints.pop();
+  std::cout << "2. size: " << myints.size() << '\n';
+
+}
+
+void stack_top()
+{
+TESTED_NAMESPACE::stack<int> mystack;
+
+  mystack.push(10);
+  mystack.push(20);
+
+  mystack.top() -= 5;
+
+  std::cout << "mystack.top() is now " << mystack.top() << '\n';
+
+}
 int main(void)
 {
-	void construct_test();
-	void assign_test();
-	void at_test()
-	void back_test()
-	void cbegin_test();
-void clear_test()
-void empty_test()
-void end_test()
-void erase_test()
-void allocator_test()
-void insert_test()
+	std::cout << "TEST VECTOR " << std::endl;
 
-
-
-
-
+	construct_test();
+	assign_test();
+	at_test();
+	back_test();
+	cbegin_test();
+	clear_test();
+	empty_test();
+	end_test();
+	erase_test();
+	allocator_test();
+	insert_test();
+	max_size_test();
+	operator_equal_test();
+	operator_cage_test();
+	pop_back_test();
+	push_back_test();
+	rbegin_test();
+	rend_test();
+	reserve_test();
+	swap_test();
+	std::cout << "TEST STACK " << std::endl;
+	stack_empty();
+	stack_pop();
+	stack_push();
+	stack_size();
+	stack_top();
+	std::cout << "TEST MAP" << std::endl;
+	return (0);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
